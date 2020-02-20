93d06c866afc46c4c375d31c9ed1db97e091c6ab
GJoaco <60699348+GJoaco@users.noreply.github.com>
2020-02-20 14:03:09 -0300

    Add files via upload

---
+++ HTML Curso/PiedraPapelTijera.js
@@ -0,0 +1,9 @@
+function inscribirse() 
+{
+	var nombre;
+	var edad;
+	nombre=document.getElementById('nombre').value;
+	edad=document.getElementById('edad').value;
+
+	alert("Ya estas inscripto "+nombre);
+}
